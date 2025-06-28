da7a5d7389413a42ddc2bcd0e9c4ba6e9d8400ce
wilsonlogo <2023ugcs019@nitjsr.ac.in>
2025-06-28 20:26:54 +0530

    Add userName variable to App component and update rendering in main.jsx

--- vite_1_basic/src/App.jsx
+++ vite_1_basic/src/App.jsx
@@ -4,13 +4,14 @@
 
 
 function App() {
-  
+  const userName="Naruto uzumaki"
 
   return (
     <>
       <h1>Hello, World!</h1>
       <p>This is a simple React application.</p>
       <p>Welcome to the Vite React setup!</p>
+      <p>{userName}</p>
     </>
   )
 }
--- vite_1_basic/src/main.jsx
+++ vite_1_basic/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <App />
+// )
+
+// Different way to render the app 
+function RenderApp(){
+  return (
+    <>
+      <h3>Rendering App using React !</h3>
+    </>
+  )
+}
+const customElement =(
+  <a href="https://vitejs.dev/guide/features.html#jsx">Vite Docs</a>
 )
+const anotherElement=React.createElement(
+  'a',
+  {href:'https://www.google.com', target:'_blank'},
+  'Click me to go to Google'
+)
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App/>
+  // RenderApp(),
+  // customElement
+  // anotherElement
+)
+
+
